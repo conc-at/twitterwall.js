0be9b932c8a98992288c10113b7196de66c2eca8
Christoph Witzko <github@christophwitzko.com>
2014-11-18 23:40:46 +0100

    feat: implemented block, tweet and flash

--- test/simpletest.js
+++ test/simpletest.js
@@ -2,3 +2,33 @@
 
 var twitterwall = require('../')({username: 'admin', password: '123456', server: 'http://localhost:5000'})
 
+function testBlocking(){
+  twitterwall.block('Test', function(err, data){
+    if(err) return console.log(err)
+    console.log(data)
+    twitterwall.block('test2', function(err, data){
+      if(err) return console.log(err)
+      console.log(data)
+      twitterwall.clearBlock('test', function(err, data){
+        if(err) return console.log(err)
+        console.log(data)
+      })
+    })
+  })
+}
+
+function tweeting(){
+  twitterwall.tweet('christophwitzko', '#lol', function(err){
+    if(err) return console.log(err)
+    console.log('ok')
+  })
+}
+
+function flashing(){
+  twitterwall.flashTimeout({}, 500, function(err){
+    if(err) return console.log(err)
+    console.log('ok')
+  })
+}
+
+testBlocking()
